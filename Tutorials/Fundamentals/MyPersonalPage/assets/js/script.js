0deec6d7387b6bf367d6c1f19152d7cd2fe56925
Ivan Orozco <ivan.oroz34@gmail.com>
2024-07-30 00:54:46 -0500

    :sparkles: Added new amazing features 🤠!!!

---
+++ Tutorials/Fundamentals/MyPersonalPage/assets/js/script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+document.addEventListener('.', function() {
+    document.getElementById('welcome').addEventListener('click', function() {
+        window.location.href = '../pages/welcome.html';
+    });
+});

f5bcc8ebe2a8dcf12e8f2abc23e371893881866e
Ivan Orozco <ivan.oroz34@gmail.com>
2024-07-31 12:12:59 -0500

    :sparkles: Added new amazing features 🤠!!!

--- Tutorials/Fundamentals/MyPersonalPage/assets/js/script.js
+++ Tutorials/Fundamentals/MyPersonalPage/assets/js/script.js
@@ -1,7 +1,17 @@
 'use strict'
 
-document.addEventListener('.', function() {
-    document.getElementById('welcome').addEventListener('click', function() {
-        window.location.href = '../pages/welcome.html';
+// Experiencia laboral, de estudios, premios, producción intelectual Al seleccionar una sección en el índice, el navegador debe deslizarse suavemente hacia dicha sección
+// en resume,  al  cliquear el indice (lista de referencias que van al id) se desplaza suavemente hacia la sección correspondiente
+
+// <li><a href="#formacion-academica" class="navbar-resume-link">Formación académica</a></li>
+document.querySelectorAll('.navbar-resume-link').forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const target = event.target.getAttribute('href'); // '#formacion-academica'
+        const targetElement = document.querySelector(target); // <section id="formacion-academica">...</section>
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });
